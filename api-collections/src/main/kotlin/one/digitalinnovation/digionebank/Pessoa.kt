b58979cd6479c20082fcf1c2412706fab9cf4c21
João Lima <jmflima@gmail.com>
2021-03-18 16:17:08 -0300

    Membros de uma Classe

--- api-collections/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,12 +3,18 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "João Lima"
     var cpf: String = "123.452.331-25"
+    var numero: Int = 60
+
     private set //impede a alteração
 
 //    inner class Endereco {
 //        var rua: String = "Rua Manoel Arruda Cavalcanti"
 //    }
 
+    constructor()
+
+    fun pessoaInfo1() = "$nome e $cpf"
+    fun pessoaInfo2() = println(nome + " mais " + cpf + " - " + numero)
 }
 
 fun main(){
@@ -17,4 +23,7 @@ fun main(){
 
     println(joao.nome)
     println(joao.cpf)
+
+    println(joao.pessoaInfo1())
+    joao.pessoaInfo2()
 }

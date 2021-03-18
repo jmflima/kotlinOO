762414e1cbd48fde8f048279c664c7605e1a0b28
João Lima <jmflima@gmail.com>
2021-03-18 05:56:15 -0300

    Foi criada a classe Conta

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/digionebank/Conta.kt
@@ -0,0 +1,19 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+class Conta(
+    val agencia: String,
+    val contaCorrente: String,
+    val Saldo: BigDecimal
+
+) {
+
+    fun deposito(valor: BigDecimal){
+
+    }
+
+    fun saque(valor: BigDecimal){
+
+    }
+}

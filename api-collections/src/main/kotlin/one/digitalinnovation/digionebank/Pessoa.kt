a903ecc384562f8bb4361a4d0a1590f99effac4e
João Lima <jmflima@gmail.com>
2021-03-20 11:25:25 -0300

    Entendendo Abstrações 2 - Tornando Funcionario e Pessoa Classes Abstratas

--- api-collections/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,29 +1,7 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "João Lima"
-    var cpf: String = "123.452.331-25"
-    var numero: Int = 60
+abstract class Pessoa (
+    val nome: String,
+    val cpf: String
+)
 
-    private set //impede a alteração
-
-//    inner class Endereco {
-//        var rua: String = "Rua Manoel Arruda Cavalcanti"
-//    }
-
-    constructor()
-
-    fun pessoaInfo1() = "$nome e $cpf"
-    fun pessoaInfo2() = println(nome + " mais " + cpf + " - " + numero)
-}
-
-fun main(){
-
-    val joao = Pessoa()
-
-    println(joao.nome)
-    println(joao.cpf)
-
-    println(joao.pessoaInfo1())
-    joao.pessoaInfo2()
-}
